f9e9109f41357fdc6d28354f3406fe4111fefd16
Vikas Garg <50917307+vgargatgit@users.noreply.github.com>
2026-08-13 08:26:13 +0530

    Add Day 6 practice five

---
+++ day-06-practice-5.js
@@ -0,0 +1,4 @@
+day6.practice.push(
+  'At zero, does ReLU have an ordinary derivative in pure mathematics? Answer: no. A software library must choose a convention for that point.',
+  'If X has shape 8 by 6 and G has shape 8 by 4, what shape does transpose(X) times G have? Answer: 6 by 4.'
+);
